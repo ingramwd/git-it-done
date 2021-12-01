72813f5bba2beb00c7523a02f2797c5cb1a31206
ingramwd <Ingramwd01@gmail.com>
2021-11-30 20:52:46 -0500

    added feature to display list-issues onto the page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,4 @@
-
+var issueContainerEl = document.querySelector('#issues-container');
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,12 +7,52 @@ var getRepoIssues = function (repo) {
         // request was successful
         if (response.ok) {
             response.json().then(function (data) {
-                console.log("data");
+                // pass response data to dom function
+                displayIssues(data);
             });
         }
         else {
-            alert("There was a problem with your request!");
+            alert("There was a problem with your code!");
         }
     });
+};
 
-}
+getRepoIssues("ingramwd/My-first-repo");
+
+var displayIssues = function (issues) {
+
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // create a span to hold the issues
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to a container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        // append to container
+        issueEl.appendChild(typeEl);
+
+        issueContainerEl.appendChild(issueEl);
+    }
+};
